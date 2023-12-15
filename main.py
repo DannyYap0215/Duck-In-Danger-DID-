df6d673d5845012384f68a076c735e35032ee46c
DannyYap0215 <dannychiyi@gmail.com>
2023-12-15 22:58:07 +0800

    changed window size to 1600 x 1000

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ pygame.display.set_caption("Duck On The Run!")
 
 #fps,sizing
 FPS = 60
-WIDTH , HEIGHT = (1200,800)
+WIDTH , HEIGHT = (1600,1000)
 BLACK = (0,0,0)
 screen = pygame.display.set_mode((WIDTH , HEIGHT))
 
@@ -21,12 +21,12 @@ background_surface = pygame.image.load("graphics/bg.png")
 duck_sprite_sheet_image = pygame.image.load("graphics/white-duck/white-duck-walk.png").convert_alpha()
 #means sprite_sheet runs through the module"duck.py" and run through the class "DuckSpriteSheet" and run through the variable "duck_sprite_sheet_image"
 sprite_sheet = spritesheet.DuckSpriteSheet(duck_sprite_sheet_image)
-duck_frame_0 = sprite_sheet.get_image(0 , 40 , 40 , 5 , BLACK)
-duck_frame_1 = sprite_sheet.get_image(1 , 40 , 40 , 5 , BLACK)
-duck_frame_2 = sprite_sheet.get_image(2 , 40 , 40 , 5 , BLACK)
-duck_frame_3 = sprite_sheet.get_image(3 , 40 , 40 , 5 , BLACK)
-duck_frame_4 = sprite_sheet.get_image(4 , 40 , 40 , 5 , BLACK)
-duck_frame_5 = sprite_sheet.get_image(5 , 40 , 40 , 5 , BLACK)
+white_duck_frame_0 = sprite_sheet.get_image(0 , 40 , 40 , 2.5 , BLACK)
+white_duck_frame_1 = sprite_sheet.get_image(1 , 40 , 40 , 2.5 , BLACK)
+white_duck_frame_2 = sprite_sheet.get_image(2 , 40 , 40 , 2.5 , BLACK)
+white_duck_frame_3 = sprite_sheet.get_image(3 , 40 , 40 , 2.5 , BLACK)
+white_duck_frame_4 = sprite_sheet.get_image(4 , 40 , 40 , 2.5 , BLACK)
+white_duck_frame_5 = sprite_sheet.get_image(5 , 40 , 40 , 2.5 , BLACK)
 
 def main(screen) :
     clock = pygame.time.Clock()
@@ -36,12 +36,15 @@ def main(screen) :
         clock.tick(FPS)
 
         screen.blit(background_surface,(0,0))
-        screen.blit(duck_frame_0,(0,0))
-        screen.blit(duck_frame_1,(200,0))
-        screen.blit(duck_frame_2,(400,0))
-        screen.blit(duck_frame_3,(600,0))
-        screen.blit(duck_frame_4,(800,0))
-        screen.blit(duck_frame_5,(1000,0))
+        
+        screen.blit(white_duck_frame_0,(0,0))
+        screen.blit(white_duck_frame_1,(200,0))
+        screen.blit(white_duck_frame_2,(400,0))
+        screen.blit(white_duck_frame_3,(600,0))
+        screen.blit(white_duck_frame_4,(800,0))
+        screen.blit(white_duck_frame_5,(1000,0))
+        
+        
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT :
