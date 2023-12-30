cafab81d205e6a2ac614ef74f9e4c908e26f0f9d
DannyYap0215 <dannychiyi@gmail.com>
2023-12-30 17:49:51 +0800

    added esc function to the game and option menu! finally !

--- game_module.py
+++ game_module.py
@@ -2,43 +2,34 @@ import pygame
 from animation import AnimationController
 from player import Player
 
-#MAIN
 def start_button(screen):
-    start_button_rect = pygame.Rect(740, 350,200,100)
+    start_button_rect = pygame.Rect(740, 350, 200, 100)
     pygame.draw.rect(screen, (0, 0, 0), start_button_rect)
     font_size = 72
-    font_name = pygame.font.Font("fonts/8-BIT WONDER.ttf",font_size)
+    font_name = pygame.font.Font("fonts/8-BIT WONDER.ttf", font_size)
     text = font_name.render("Start", True, (255, 255, 255))
     text_rect = text.get_rect(center=start_button_rect.center)
     screen.blit(text, text_rect)
-    
+
 def option_button(screen):
     option_button_rect = pygame.Rect(740, 450, 200, 100)
     pygame.draw.rect(screen, (0, 0, 0), option_button_rect)
     font_size = 72
-    font_name = pygame.font.Font("fonts/8-BIT WONDER.ttf",font_size)
+    font_name = pygame.font.Font("fonts/8-BIT WONDER.ttf", font_size)
     text = font_name.render("Option", True, (255, 255, 255))
     text_rect = text.get_rect(center=option_button_rect.center)
     screen.blit(text, text_rect)
-    
+
 def quit_button(screen):
     quit_button_rect = pygame.Rect(740, 550, 200, 100)
     pygame.draw.rect(screen, (0, 0, 0), quit_button_rect)
     font_size = 72
-    font_name = pygame.font.Font("fonts/8-BIT WONDER.ttf",font_size)
+    font_name = pygame.font.Font("fonts/8-BIT WONDER.ttf", font_size)
     text = font_name.render("Quit", True, (255, 255, 255))
     text_rect = text.get_rect(center=quit_button_rect.center)
     screen.blit(text, text_rect)
-    
-def resume_button(screen):
-    resume_button_rect = pygame.Rect(740, 650, 200, 100)
-    pygame.draw.rect(screen, (0, 0, 0), resume_button_rect)
-    font_size = 72
-    font_name = pygame.font.Font("fonts/8-BIT WONDER.ttf",font_size)
-    text = font_name.render("Resume", True, (255, 255, 255))
-    text_rect = text.get_rect(center=resume_button_rect.center)
-    screen.blit(text, text_rect)
 
+    
 
 def main(screen):
     clock = pygame.time.Clock()
@@ -46,7 +37,9 @@ def main(screen):
     player = Player(400, 300)  # Adjust the initial position as needed
     background = pygame.image.load("graphics/background1.png").convert()
 
-    show_home_screen = True
+    show_menu_screen = True
+    show_game_screen = False
+    show_option_screen = False
     selected_button = 0  # 0 is start, 1 is option, 2 is quit
 
     selected_button_blink_timer = 0
@@ -56,7 +49,7 @@ def main(screen):
     while True:
         clock.tick(60)
 
-        if show_home_screen:
+        if show_menu_screen:
             screen.fill((0, 0, 0))
             start_button(screen)
             option_button(screen)
@@ -70,7 +63,16 @@ def main(screen):
             if is_button_visible:
                 selection_button = pygame.Rect(540, 350 + selected_button * 100, 30, 100)
                 pygame.draw.rect(screen, (255, 255, 255), selection_button)
-        else:
+        elif show_option_screen :
+            screen.fill((0, 0, 0))  # Black screen for the option screen
+            font_size = 72
+            font_name = pygame.font.Font("fonts/8-BIT WONDER.ttf", font_size)
+            text = font_name.render("Option Screen", True, (255, 255, 255))
+            text_rect = text.get_rect(center=(1600 // 2, 1000 // 2))
+            screen.blit(text, text_rect)
+            pygame.display.update()
+            
+        elif show_game_screen:
             screen.blit(background, (0, 0))
 
             keys = pygame.key.get_pressed()
@@ -97,25 +99,36 @@ def main(screen):
                 pygame.quit()
                 exit()
             elif event.type == pygame.KEYDOWN:
-                if show_home_screen and event.key == pygame.K_RETURN:
-                    if selected_button == 0:  # Start game
-                        show_home_screen = False
-                        animation_controller.setup_animation()
-                    elif selected_button == 1:  # Option
-                        pass
-                    elif selected_button == 2:  # Quit
-                        pygame.quit()
-                        exit()
+                if event.key == pygame.K_RETURN:
+                    if show_menu_screen:
+                        if selected_button == 0:  # Start game
+                            show_menu_screen = False
+                            show_game_screen = True
+                            animation_controller.setup_animation()
+                        elif selected_button == 1:  # Option
+                            show_menu_screen = False
+                            show_option_screen = True
+                        elif selected_button == 2:  # Quit
+                            pygame.quit()
+                            exit()
+                        elif event.key == pygame.K_ESCAPE:
+                                show_option_screen = False
+                                show_menu_screen = True
                 elif event.key == pygame.K_DOWN:
                     if selected_button < 2:
                         selected_button += 1
                 elif event.key == pygame.K_UP:
                     if selected_button > 0:
                         selected_button -= 1
+                elif event.key == pygame.K_ESCAPE:
+                    show_menu_screen = True
+                    show_game_screen = False
+                    show_option_screen = False
+                
 
 if __name__ == "__main__":
     pygame.init()
     pygame.display.set_caption("Duck On The Run!")
     WIDTH, HEIGHT = (1600, 1000)
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    main(screen) 
+    main(screen)
